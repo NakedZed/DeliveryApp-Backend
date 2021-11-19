94fcf0a3dbf005a55d42246b5b0f79c51cf6e536
Ahmed Reda <manoa15@yahoo.com>
2021-11-19 12:57:54 +0200

    change path of storage

--- utils/firebaseConfiguration.js
+++ utils/firebaseConfiguration.js
@@ -3,7 +3,7 @@ const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage({
   projectId: 'delivery-app-5e621',
-  keyFilename: 'delivery-app-5e621-firebase-adminsdk-kjin7-465d741a9b.json',
+  keyFilename: '../delivery-app-5e621-firebase-adminsdk-kjin7-465d741a9b.json',
 });
 let bucket = storage.bucket('gs://delivery-app-5e621.appspot.com');
 
