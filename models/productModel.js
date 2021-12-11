6ebb2b6644de3e9090d0e1bfeb73d4987593d82e
Ahmed Reda <manoa15@yahoo.com>
2021-12-11 20:56:58 +0200

    Add product description

--- models/productModel.js
+++ models/productModel.js
@@ -15,6 +15,9 @@ const productSchema = new mongoose.Schema({
     type: Number,
   },
   photo: { type: String },
+  description: {
+    type: String,
+  },
 });
 const Product = new mongoose.model('Product', productSchema);
 module.exports = Product;
